cbb1c143a3830a03dbd701b3a3c201b434303ea8
Paul Jones <pjones@ivycollegiate.org>
2022-12-12 07:50:23 +0000

    Added Hermione to Gryffindor and printed in console

--- assets/dict.py
+++ assets/dict.py
@@ -3,3 +3,9 @@ houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
 
 #This is how you print out the dict in a console
 print(houses["Harry"])
+
+#This is how you add values to the dictionary:
+houses["Hermione"]= "Gryffindor"
+
+#This is how you print out Hermione's House
+print(houses["Hermione"])
